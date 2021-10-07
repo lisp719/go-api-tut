435876da6ebe46b69b807e012000e1b15c85514a
lisp719 <368034+lisp719@users.noreply.github.com>
2021-10-07 04:54:33 +0000

    feat: Implement user crud api

--- core/router.go
+++ core/router.go
@@ -1,22 +1,59 @@
 package core
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 
 	r.GET("/users", func(c *gin.Context) {
-		users := Db.Find(&[]User{})
+		users := []User{}
+		Db.Find(&users)
 
-		c.JSON(200, gin.H{
-			"count": users.RowsAffected,
-		})
+		c.JSON(http.StatusOK, users)
+	})
+
+	r.POST("/users", func(c *gin.Context) {
+		user := User{Name: c.PostForm("name")}
+		Db.Create(&user)
+
+		c.JSON(http.StatusCreated, user)
+	})
+
+	r.GET("/users/:id", func(c *gin.Context) {
+		user := User{}
+		Db.First(&user, c.Param("id"))
+
+		c.JSON(http.StatusOK, user)
+	})
+
+	r.PUT("/users/:id", func(c *gin.Context) {
+		user := User{}
+		Db.First(&user, c.Param("id"))
+
+		err := c.BindJSON(&user)
+		if err != nil {
+			return
+		}
+
+		Db.Save(&user)
+
+		c.JSON(http.StatusOK, user)
+	})
+
+	r.DELETE("/users/:id", func(c *gin.Context) {
+		Db.Delete(&User{}, c.Param("id"))
+
+		c.JSON(http.StatusNoContent, nil)
 	})
 
 	return r
