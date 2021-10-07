35413e68372195d83ad1d20d8462907c7111d326
lisp719 <368034+lisp719@users.noreply.github.com>
2021-10-07 09:35:27 +0000

    feat: Allow cors access

--- core/router.go
+++ core/router.go
@@ -3,11 +3,14 @@ package core
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.Default())
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
